81e169ab2ffdaa143cb278d2267375b8a7784d10
Anand Patel <anandpatel@braindo-anandp.local>
2018-07-14 14:31:40 -0400

    added in desktop component, minitile component, and chart.js

--- src/Desktop.js
+++ src/Desktop.js
@@ -1,15 +1,63 @@
 import React, { Component } from 'react';
+import MiniTile from './MiniTile';
+import {Line} from 'react-chartjs-2';
+import './Desktop.css'
+
 
 class Desktop extends Component {
-
-  constructor(){
+  constructor(props){
     super();
   }
 
+  parseTime(time) {
+    return (new Date(time*1000).getHours())%12;
+  }
+
   render() {
+    let data = {
+      labels: '',
+      datasets: [{
+        label: 'Hourly',
+        fill: true,
+        lineTension: 0.1,
+        backgroundColor: 'rgba(244,72,66,0.4)',
+        borderColor: 'rgba(244,72,66,1)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: 'rgba(244,72,66,1)',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgba(244,72,66,1)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: ''
+      }]
+    };
+
+    data.labels = this.props.hourly.data.slice(0,25).map((hour)=>{return this.parseTime(hour.time)});
+    data.datasets[0].data = this.props.hourly.data.map((hour)=>{return hour.temperature});
+
     return (
-      <div className="desktop">
-        <p>Desktop!</p>
+      <div className="desktop-tile">
+        <div className="top-container">
+          <div className="currently">
+            <span className='desktop-temp'>It is currently {Math.round(this.props.currently.temperature)}°f</span>
+            <span className='desktop-summary'>{this.props.hourly.summary}</span>
+          </div>
+        </div>
+        <div className="chart">
+          <Line data={data} height={120} />
+        </div>
+        <div className="mini-tiles">
+          {this.props.daily.data.map((day, index) => {
+                return <MiniTile data={day} place={index} key={index}/> // This line doesn't work when wrapped in brackets. Why?
+          })}
+        </div>
       </div>
     );
   }
--- src/Weather.js
+++ src/Weather.js
@@ -16,7 +16,12 @@ class Weather extends Component {
                       temperatureLow: 0,
                     icon: ''}],
               summary: ''},
-      formatted_address: ''
+      formatted_address: '',
+      currently: {temperature: ''},
+      hourly:{data:[0],
+                summary: '',
+                time: ''
+              }
     };
     this.grabForecast(this.props.coords);
     this.grabZipInfo(this.props.zip);
@@ -61,16 +66,16 @@ class Weather extends Component {
           <div className='summary'>{this.state.daily.summary}</div>
         </div>
         <div className='middle'>
-          <MediaQuery maxDeviceWidth={1224}>
-            <Slider ref={c => (this.slider = c)} {...sliderSettings}>
+          <MediaQuery maxDeviceWidth={924}>
+            <Slider {...sliderSettings}>
               {this.state.daily.data.map((day, index) => 
                 <Tile data={day} place={index} key={index}/> // This line doesn't work when wrapped in brackets. Why?
               )}
             </Slider>
           </MediaQuery>
-          <MediaQuery minDeviceWidth={1224}>
+          <MediaQuery minDeviceWidth={925}>
             <div className='desktop'>
-              <Desktop props={{...this.state}}/> 
+              <Desktop {...this.state}/> 
             </div>
           </MediaQuery>
         </div>
