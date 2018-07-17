53ef9582e5a16c1cc50a916a7c2149557235565d
Anand Patel <anandpatel@braindo-anandp.local>
2018-07-17 15:20:30 -0400

    split graph out into it's own component

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 class App extends Component {
   render() {
     return (
-        <Router>
-          <div className='App'>
-              <Route exact path='/' component={Prompt}/>
-              <Route path='/:coords/:zip' component={({match}) => <Weather coords={match.params.coords} zip={match.params.zip}/>}/>
-          </div>
-        </Router>
+      <Router>
+        <div className='App'>
+          <Route exact path='/' component={Prompt}/>
+          <Route path='/:coords/:zip' component={({match}) => <Weather coords={match.params.coords} zip={match.params.zip}/>}/>
+        </div>
+      </Router>
     );
   }
 }
